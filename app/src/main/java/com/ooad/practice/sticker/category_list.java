841adee28459de53f63cf22c891eb1c431ca7710
102820021 <102820021wsx987@gmail.com>
2017-05-08 16:19:27 +0800

    merge code

--- app/src/main/java/com/ooad/practice/sticker/category_list.java
+++ app/src/main/java/com/ooad/practice/sticker/category_list.java
@@ -9,24 +9,31 @@ import android.widget.AdapterView.OnItemClickListener;
 import android.view.*;
 import android.widget.AdapterView;
 
+import com.ooad.practice.sticker.Bean.*;
+import com.ooad.practice.sticker.Controller.CategoryHandler;
+
+import java.util.List;
+
 import Adapter.MyAdapter;
 
 public class category_list extends ActionBarActivity {
-    private ListView listView01;
+    private List<Category> categoryList;
+    private CategoryHandler handler = new CategoryHandler();
+    private ListView listView;
     private String[] show_text = {"未帶鑰匙","網路問題","電腦故障","商店","診所","未帶鑰匙","網路問題","電腦故障","商店","診所"};
     private int[] isButtonVisible = {1, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-    private ArrayAdapter listAdapter;
-    private TextView textView01;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category_list);
 
-        listView01 = (ListView)findViewById(R.id.listView1);
+        categoryList = handler.getCategoryList("");
+
+        listView = (ListView)findViewById(R.id.listView1);
 //        listAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,show_text);
-        listView01.setAdapter(new MyAdapter(category_list.this, show_text, isButtonVisible));
-        listView01.setOnItemClickListener(new OnItemClickListener(){
+        listView.setAdapter(new MyAdapter(category_list.this, show_text, isButtonVisible));
+        listView.setOnItemClickListener(new OnItemClickListener(){
             @Override
             public void onItemClick(AdapterView parent, View view, int position, long id){
                 Toast.makeText(getApplicationContext(),
