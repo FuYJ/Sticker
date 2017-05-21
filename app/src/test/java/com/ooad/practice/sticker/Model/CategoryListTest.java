1cbe6ab1f21fea6683071a06ac6a2f07d48f3a14
FuYJ <zz28234830@gmail.com>
2017-05-21 23:16:36 +0800

    Add jmock packages

---
+++ app/src/test/java/com/ooad/practice/sticker/Model/CategoryListTest.java
@@ -0,0 +1,45 @@
+package com.ooad.practice.sticker.Model;
+
+import org.jmock.Mockery;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by fuyiru on 2017/5/21.
+ */
+public class CategoryListTest {
+    @Before
+    public void setUp() throws Exception {
+        Mockery context = new Mockery();
+        
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void getInstance() throws Exception {
+
+    }
+
+    @Test
+    public void getCategoryList() throws Exception {
+
+    }
+
+    @Test
+    public void setCategory() throws Exception {
+
+    }
+
+    @Test
+    public void deleteCategory() throws Exception {
+
+    }
+
+}
---
+++ app/src/test/java/com/ooad/practice/sticker/Model/StickerListTest.java
@@ -0,0 +1,68 @@
+package com.ooad.practice.sticker.Model;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by fuyiru on 2017/5/21.
+ */
+public class StickerListTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void getInstance() throws Exception {
+
+    }
+
+    @Test
+    public void getStickerListByCategoryId() throws Exception {
+
+    }
+
+    @Test
+    public void getStickerList() throws Exception {
+
+    }
+
+    @Test
+    public void setSticker() throws Exception {
+
+    }
+
+    @Test
+    public void deleteSticker() throws Exception {
+
+    }
+
+    @Test
+    public void getEmergentList() throws Exception {
+
+    }
+
+    @Test
+    public void getLatestStickers() throws Exception {
+
+    }
+
+    @Test
+    public void calculateDate() throws Exception {
+
+    }
+
+    @Test
+    public void calculateDate1() throws Exception {
+
+    }
+
+}
